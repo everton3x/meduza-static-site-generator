2adf910b76e523cf14b19364049a9d2b490d6933
Everton da Rosa <everton3x@gmail.com>
2020-05-27 09:15:30 -0300

    composer update

---
+++ src/Meduza/Build/BuildRepo.php
@@ -0,0 +1,12 @@
+<?php
+namespace Meduza\Build;
+
+/**
+ * Repositório dos dados durante a contrução do conteúdo estático.
+ *
+ * @author Everton
+ */
+class BuildRepo
+{
+    //put your code here
+}
